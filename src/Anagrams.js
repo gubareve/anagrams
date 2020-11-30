6f27f739ca9653f896b1a2969e2ed0b53f8a77ea
Evan Gubarev <evan@evangubarev.com>
2020-11-30 13:55:35 -0800

    first commit

---
+++ src/Anagrams.js
@@ -0,0 +1,20 @@
+import words from './words.js';
+
+function *permute(a, n = a.length) {
+  if (n <= 1) yield a.slice();
+  else for (let i = 0; i < n; i++) {
+    yield *permute(a, n - 1);
+    const j = n % 2 ? 0 : i;
+    [a[n-1], a[j]] = [a[j], a[n-1]];
+  }
+}
+
+export default function findAnagrams(Input) {
+  Input = Input.toLowerCase();
+  let permutations = Array.from(permute(Input.split('')))
+  .map(perm => perm.join(''))
+  .filter((el, idx, self) => (self.indexOf(el) === idx))  
+  return (
+    permutations.filter(value=>words.includes(value))
+  );
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React from 'react';
+import SubtitlesIcon from '@material-ui/icons/Subtitles';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Output from './Output.js';
+import findAnagrams from './Anagrams.js';
+import ErrorAlert from './Errors.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AnagramFinder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'One' };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+
+  render() {
+    if (this.state.value.length < 7 && this.state.value.length !== 0) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SubtitlesIcon className="Subtitles"/>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="TextField">
+          <TextField id="outlined-basic" label="Enter text here." variant="outlined" onChange={this.handleChange} defaultValue={this.state.value} />
+          </div>
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <div className="Output">
+        <Output className="Output" Input={findAnagrams(this.state.value)} />
+        </div>
+        </header>
+      </div>
+    );
+  } else if (this.state.value.length === 0) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SubtitlesIcon className="Subtitles"/>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="TextField">
+          <TextField error helperText="Please enter a word!" id="outlined-basic" label="Enter text here." variant="outlined" onChange={this.handleChange} defaultValue={this.state.value} />
+          </div>
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <div className="Output">
+          <ErrorAlert message="Please enter a word!"/>
+        </div>
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SubtitlesIcon className="Subtitles"/>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="TextField">
+          <TextField error helperText="Too many letters!" id="outlined-basic" label="Enter text here." variant="outlined" onChange={this.handleChange} defaultValue={this.state.value} />
+          </div>
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <div className="Output">
+          <ErrorAlert message="Too many letters!" />
+        </div>
+        </header>
+      </div>
+    );
+  }
+}
 }
 
-export default App;
+export default AnagramFinder;
